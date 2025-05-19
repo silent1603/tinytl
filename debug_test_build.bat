da1104a9adc636b075faf8d1fb2b7737fe6fb14a
silent1603 <tamgiangbui@gmail.com>
2025-05-19 22:07:22 +0700

    * update time count

--- debug_test_build.bat
+++ debug_test_build.bat
@@ -29,8 +29,8 @@ pushd "%source_dir%"
 (for %%f in (*.cpp) do echo "%source_dir%\%%f") > "%cpp_list%"
 popd
 
-:: === Start timer ===
-for /f %%t in ('powershell -nologo -command "Get-Date -UFormat %%s"') do set start=%%t
+:: === Start Timer ===
+set "startTime=%time: =0%"
 
 :: === Compile step ===
 echo Compiling...
@@ -54,10 +54,17 @@ if errorlevel 1 (
     exit /b 1
 )
 
-:: === End timer ===
-for /f %%t in ('powershell -nologo -command "Get-Date -UFormat %%s"') do set end=%%t
-set /a duration=%end% - %start%
-
+:: === End Timer ===
+set "endTime=%time: =0%"
 echo Build successful: %build_dir%\%exe_name%
-echo Build time: %duration% seconds
+:: === Get elapsed time: ===
+set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+
+rem Convert elapsed time to HH:MM:SS:CC format:
+set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+
+echo Start:    %startTime%
+echo End:      %endTime%
+echo Elapsed:  %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
 pause
